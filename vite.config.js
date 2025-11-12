9fd96c76a6d63b4f1d105efdc91b0b9d0f7b5e89
salem <salem>
2025-11-12 21:09:28 +0100

    web v1

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Melon-book/melon-client-test/',
+  base: '/melon-client-test/',
   plugins: [react()],
 })
